d4e6afdfda1d70de145c792a969ce7c04947e550
miku3920 <papa964204@gmail.com>
2021-03-05 15:51:25 +0800

    cookie price

---
+++ myproject/20210305/013.py
@@ -0,0 +1,15 @@
+a = 1
+b = 1
+c = 1
+
+if a == 1:
+    print("a==1")
+else:
+    print("a not 1")
+
+if a == 1:
+    print("a==1")
+elif b == 1:
+    print("b==1")
+else:
+    print("a and b not 1")
---
+++ myproject/20210305/014.py
@@ -0,0 +1,8 @@
+cookiePrice = 20
+unit = 10
+
+price = unit * cookiePrice
+if price > 100:
+    price *= 0.9
+    
+print(price)
